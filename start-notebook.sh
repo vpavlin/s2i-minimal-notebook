75934915e69f7cf47cac50e6471b99ff58df35e9
Landon LaSmith <LLaSmith@redhat.com>
2020-02-26 14:09:40 -0500

    Add support for cloning branch repo
    Notebook pod env var "JUPYTER_PRELOAD_REPOS" can now specify a branch
    based on format "https://<GIT_REPO_URL>#<GIT_BRANCH_NAME>"
    
    Signed-off-by: Landon LaSmith <LLaSmith@redhat.com>

--- start-notebook.sh
+++ start-notebook.sh
@@ -41,6 +41,27 @@ fi
 
 JUPYTER_PROGRAM_ARGS="$JUPYTER_PROGRAM_ARGS --config=/opt/app-root/etc/jupyter_notebook_config.py"
 
+if [ -n "${JUPYTER_PRELOAD_REPOS}" ]; then
+    for repo in `echo ${JUPYTER_PRELOAD_REPOS} | tr ',' ' '`; do
+        # Check for the presence of "@branch" in the repo string
+        REPO_BRANCH=$(echo ${repo} | cut -s -d'@' -f2)
+        if [[ -n ${REPO_BRANCH} ]]; then
+          # Remove the branch from the repo string and convert REPO_BRANCH to git clone arg
+          repo=$(echo ${repo} | cut -d'@' -f1)
+          REPO_BRANCH="-b ${REPO_BRANCH}"
+        fi
+        echo "Checking if repository $repo exists locally"
+        REPO_DIR=$(basename ${repo})
+        if [ -d "${REPO_DIR}" ]; then
+            pushd ${REPO_DIR}
+            GIT_SSL_NO_VERIFY=true git pull --ff-only
+            popd
+        else
+            GIT_SSL_NO_VERIFY=true git clone ${repo} ${REPO_DIR} ${REPO_BRANCH}
+        fi
+    done
+fi
+
 if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
     if [[ "$JUPYTER_ENABLE_LAB" =~ ^(true|yes|y|1)$ ]]; then
         JUPYTER_PROGRAM="jupyter labhub"
